c1bbf49bae7126f6403c4627cd95ba4706d375d1
KirillSpitsyn <zima197211@gmail.com>
2025-04-08 07:23:51 -0400

    Update LocationList.tsx

--- src/components/LocationList.tsx
+++ src/components/LocationList.tsx
@@ -87,7 +87,7 @@ const LocationList: React.FC<LocationListProps> = ({
                                 <div className="flex items-start">
                                     <div className="flex-shrink-0 mr-3">
                                         <div 
-                                            className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center text-lg"
+                                            className="w-8 h-8 bg-blue-100 rounded-full flex items-center justify-center text-lg text-black"
                                             style={{minWidth: '2rem'}}
                                         >
                                             {index + 1}
